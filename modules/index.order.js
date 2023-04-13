30bf05c64c6d3cc53a172515f623e5afc30738d7
Muxammadaminwebs <qobiljonovmuxammadamin@gmail.com>
2023-04-13 15:53:01 +0500

    add-2

--- modules/index.order.js
+++ modules/index.order.js
@@ -1,6 +1,6 @@
 import { readFileSync } from "fs";
 import resolvers from "./order.resolvers.js";
-import path from "path"
+import path from "path";
 const orderSchema=readFileSync(path.join(process.cwd(),"modules","order.schema.gql"),"utf-8")
 export default{
   orderSchema,
--- modules/order.resolvers.js
+++ modules/order.resolvers.js
@@ -21,4 +21,4 @@ const resolvers={
   }
 }
 
-export default resolvers
+export default resolvers;
